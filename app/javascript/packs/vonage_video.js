260b4170f707238c47a34e7f0dcdf8291509373b
gaurav2699 <gauravsingh2699@gmail.com>
2021-06-25 14:28:57 +0530

    added signal listener

---
+++ app/javascript/packs/vonage_video.js
@@ -0,0 +1,35 @@
+import Chat from './chat.js'
+import Party from './videochat.js'
+
+// declare empty global session variable
+var session = ''
+
+if (window.location.pathname == '/videochat') {
+    document.addEventListener('DOMContentLoaded', function() {
+        // Hide or show watch party link based on participant
+        if (name != '' && window.location.pathname == '/videochat') {
+
+            // Initialize an OpenTok Session object
+            if (session == '') {
+                session = OT.initSession(api_key, session_id);
+            }
+
+            new Chat(session);
+            new Party(session);
+
+            // Connect to the Session using a 'token'
+            session.connect(token, function(error) {
+                if (error) {
+                    console.error('Failed to connect', error);
+                }
+            });
+
+            // Listen for Signal screenshare message
+            session.on('signal:screenshare', function screenshareCallback(event) {
+                if (event.data == 'on') {
+                    window.location = '/screenshare?name=' + name;
+                };
+            });
+        };
+    });
+}
---
+++ public/packs/js/hello_react-0355f8bc1772210d7042.js
@@ -0,0 +1,78 @@
+(self["webpackChunkmicrosoft_teams_clone"] = self["webpackChunkmicrosoft_teams_clone"] || []).push([["hello_react"],{
+
+/***/ "./app/packs/entrypoints/hello_react.jsx":
+/*!***********************************************!*\
+  !*** ./app/packs/entrypoints/hello_react.jsx ***!
+  \***********************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_2__);
+var _jsxFileName = "/home/gaurav/microsoft_teams_clone/app/packs/entrypoints/hello_react.jsx",
+    _jsxFileName2 = "/home/gaurav/microsoft_teams_clone/app/packs/entrypoints/hello_react.jsx",
+    _this = undefined;
+
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+
+
+
+var Hello = function Hello(props) {
+  var _React$createElement;
+
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", (_React$createElement = {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10,
+      columnNumber: 3
+    }
+  }, _React$createElement["__self"] = _this, _React$createElement["__source"] = {
+    fileName: _jsxFileName2,
+    lineNumber: 10,
+    columnNumber: 3
+  }, _React$createElement), "Hello ", props.name, "!");
+};
+
+Hello.defaultProps = {
+  name: 'David'
+};
+Hello.propTypes = {
+  name: (prop_types__WEBPACK_IMPORTED_MODULE_2___default().string)
+};
+document.addEventListener('DOMContentLoaded', function () {
+  var _React$createElement2;
+
+  react_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(Hello, (_React$createElement2 = {
+    name: "React",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 23,
+      columnNumber: 5
+    }
+  }, _React$createElement2["__self"] = _this, _React$createElement2["__source"] = {
+    fileName: _jsxFileName2,
+    lineNumber: 23,
+    columnNumber: 5
+  }, _React$createElement2)), document.body.appendChild(document.createElement('div')));
+});
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["vendors-node_modules_prop-types_index_js-node_modules_react-dom_index_js"], function() { return __webpack_exec__("./app/packs/entrypoints/hello_react.jsx"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
+//# sourceMappingURL=hello_react-0355f8bc1772210d7042.js.map
